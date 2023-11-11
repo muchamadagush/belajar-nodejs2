5fe9016795a357bb1b0961991844250831741706
muchamadagushh <muchamadagush@gmail.com>
2023-11-11 09:27:06 +0700

    perubahan lagi di controller user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -41,6 +41,8 @@ const createUser = async (req, res) => {
         
         await User.create({ fullName, email, password: hashPassword, status: 'Active' })
 
+        // perubahan juga
+
         res.status(201).json({ message: 'User created' })
     } catch (error) {
         res.status(500).json({ message: 'Internal Server Error' })
