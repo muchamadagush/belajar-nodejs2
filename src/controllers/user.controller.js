9cfe17309618e2800909b0f0c4c7ad90606fc270
muchamadagushh <muchamadagush@gmail.com>
2023-11-16 16:39:01 +0700

    update branch post

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,6 +29,7 @@ const getAllUser = async (req, res) => {
 
     // ada perubahan
     // perubahan baru git
+    // perubahan dari branch feature post
 
     res.status(200).json({ data: user, total })
   } catch (error) {
